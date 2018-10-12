2390cc0393d017514c574e71b1a1f36d46b31387
Mike Starr <mstarr@rsui.com>
2018-10-12 10:30:23 -0400

    02-webpack-config

--- src/index.js
+++ src/index.js
@@ -2,7 +2,21 @@
 import { items } from './data.js'
 import { capitalize, head, tail } from './utils.js'
 
-console.log("hello")
 
-items.map( i => console.log(tail(i)));
+
+const div = document.createElement("div");
+div.innerText = "hello there...";
+
+const listitems = items.map( i => {
+    let li = document.createElement("li");
+    li.innerText = i;
+    return li;
+});
+
+const list = document.createElement("ul");
+
+listitems.forEach( i => list.appendChild(i) );
+
+document.body.appendChild(div);
+document.body.appendChild(list);
 
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+
+module.exports = env => {
+  console.log(env);
+
+  return (
+    {
+      mode: env.mode,
+      plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()]
+    }
+  );
+};
