1dd378c35bbe8f34e4d17658f6bb91b397e3147f
Mike Starr <mstarr@rsui.com>
2018-10-12 13:56:13 -0400

    changed exports on data.js

--- src/data.js
+++ src/data.js
@@ -1,6 +1,8 @@
-export const items = ["Item 1", "Item 2", "Item 3"];
-export const numbers = [1,2,3,4,5,6,7,8,9];
+const items = ["Item 1", "Item 2", "Item 3"];
+const numbers = [1,2,3,4,5,6,7,8,9];
 
-export const colors = ["Red", "Blue", "Green"];
+const colors = ["Red", "Blue", "Green"];
 
 const createPerson = (name, age) => ({ name: name, age: age });
+
+export { items, numbers, colors };
